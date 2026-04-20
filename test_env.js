f1bb30d2face3bf94262f594093b0d0450a36bd9
Thiago Costa <curupaco@gmail.com>
2026-04-20 00:41:31 -0300

    clean de arquivos antigos

--- test_env.js
+++
@@ -1,4 +0,0 @@
-import { loadEnv } from 'vite';
-process.env.VITE_FIREBASE_API_KEY = 'TEST_KEY_FROM_VERCEL';
-const env = loadEnv('production', '.', '');
-console.log('API_KEY in env:', env.VITE_FIREBASE_API_KEY);
--- tests/vercel_test.js
+++
@@ -1,1 +0,0 @@
-const regex = new RegExp("^/((?!assets|logo.svg|manifest.json|sw.js|favicon.ico).*)$"); console.log(regex.test("/landing")); console.log(regex.test("/assets/file.js"));
